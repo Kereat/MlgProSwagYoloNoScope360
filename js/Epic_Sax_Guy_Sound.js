15ff6c4451ed37f3aafea515e9186e42b5748426
Kereat <31939118+Kereat@users.noreply.github.com>
2017-10-28 20:50:02 +0300

    Add files via upload

---
+++ js/Epic_Sax_Guy_Sound.js
@@ -0,0 +1,48 @@
+function playSound(url){
+				var s = new Audio();
+				s.src = url;
+				s.play(); 
+			}
+
+		addEventListener("keydown", function(event) { 
+			if (event.keyCode == 48)				
+	   		 {
+				playSound("source/audio/Epic_Sax_Guy-MLG_Airhorn_Remix.mp3");	
+	   		 }
+	   		 if (event.keyCode == 49)				
+	   		 {
+				playSound("source/audio/AirHorn.mp3");	
+	   		 }
+	   		 if (event.keyCode == 50)				
+	   		 {
+				playSound("source/audio/Damn_Son_Where'd_You_Find_This.mp3");	
+	   		 }
+	   		 if (event.keyCode == 51)				
+	   		 {
+				playSound("source/audio/Explosion.mp3");	
+	   		 }
+	   		 if (event.keyCode == 52)				
+	   		 {
+				playSound("source/audio/Funny_Reactions.mp3");	
+	   		 }
+	   		 if (event.keyCode == 53)				
+	   		 {
+				playSound("source/audio/Oh_Baby_A_Triple..._Oh_Yeeah.mp3");	
+	   		 }
+	   		 if (event.keyCode == 54)				
+	   		 {
+				playSound("source/audio/Punch.mp3");	
+	   		 }
+	   		 if (event.keyCode == 55)				
+	   		 {
+				playSound("source/audio/Really_Nigga.mp3");	
+	   		 }
+	   		 if (event.keyCode == 56)				
+	   		 {
+				playSound("source/audio/Smoke_Weed_Everyday.mp3");	
+	   		 }
+	   		 if (event.keyCode == 57)				
+	   		 {
+				playSound("source/audio/Wow.mp3");	
+	   		 }
+		});
